f53355b44590ef9024d8a7de4279f8495f6471b4
tony.zhang <tony.zhang@tianjishuju.com>
2020-03-27 14:27:58 +0800

    底部导航demo

---
+++ flutter_bottomnavigationbar/lib/pages/airplay_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AirPlayScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('AirPlay'),),
+    );
+  }
+}
---
+++ flutter_bottomnavigationbar/lib/pages_keep_alive/email_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class EmailScreen extends StatefulWidget {
+  @override
+  _EmailScreenState createState() => _EmailScreenState();
+}
+
+class _EmailScreenState extends State<EmailScreen>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    super.build(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('EmailScreen'),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+}
